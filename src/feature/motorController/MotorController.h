2c9450228634a8317866a4a78309a879685b943c
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2025-03-20 15:45:25 +0100

    feat : modification algo pour moteur

---
+++ src/feature/motorController/MotorController.h
@@ -0,0 +1,42 @@
+#ifndef MOTOR_CONTROLLER_H
+#define MOTOR_CONTROLLER_H
+
+#include "driver/ledc.h"
+#include "esp_log.h"
+#include <algorithm>
+#include <ControllerRequestDTO.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define NUM_MOTORS 4
+#define TAG "MotorController"
+
+class MotorController {
+public:
+    MotorController();
+    void init();
+    void setMotorSpeed(int motorIndex, int speed); // speed: 0 - 180
+    void emergencyStop();
+    void Task();
+
+    static SemaphoreHandle_t xControllerRequestMutex;
+    static ControllerRequestDTO currentControllerRequestDTO;
+private:
+    ControllerRequestDTO lastControllerRequestDTO;
+    const int escPins[NUM_MOTORS] = {13, 12, 14, 15};  // GPIOs utilisés pour les moteurs
+    const ledc_channel_t ledcChannels[NUM_MOTORS] = {LEDC_CHANNEL_0, LEDC_CHANNEL_1, LEDC_CHANNEL_2, LEDC_CHANNEL_3};
+
+    static constexpr int PWM_FREQ = 50;     // 50Hz pour les ESC
+    static constexpr int PWM_RES = 12;      // Résolution de 12 bits (0 - 4095)
+    static constexpr int PWM_MAX = (1 << PWM_RES) - 1;
+    static constexpr int PWM_MIN = int(PWM_MAX * 0.05);     // ~5% duty cycle (1000us)
+    static constexpr int PWM_ESC_MAX = int(PWM_MAX * 0.10); // ~10% duty cycle (2000us)
+
+    int currentSpeed[NUM_MOTORS] = {0, 0, 0, 0};
+
+    bool isEmergencyStop = false;
+    
+    int calcMotorDuty(int speed);
+};
+
+#endif // MOTOR_CONTROLLER_H
